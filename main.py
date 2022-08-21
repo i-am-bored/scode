1612aff80b93101087b6903bcd7769f7ba6c25be
i-am-bored <a01043412349@gmail.com>
2022-08-21 16:50:09 +0900

    get code and send it to server

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+now_visitors = 1
+
+@app.route('/', methods=['GET','POST'])
+def mainpage():
+    global now_visitors
+    now_visitors += 1
+    return render_template('index.html', value=now_visitors)
+
+@app.route('/upload', methods=['GET','POST'])
+def upload_scode():
+    return render_template('add_scode.html')\
+
+@app.route('/post', methods=['GET','POST'])
+def post():
+    if request.method == 'POST':
+        value = request.form['thecode']
+        value = str(value)
+        print(value)
+    return render_template('post.html', value=value)
+    
+if __name__ == '__main__':
+    app.run()
