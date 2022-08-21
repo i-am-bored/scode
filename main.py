3e0bba053edc736e387b09136b9eae7008ac6a9b
i-am-bored <a01043412349@gmail.com>
2022-08-21 20:30:00 +0900

    add a page show scodes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+from replit import db
 app = Flask(__name__)
 
 now_visitors = 1
@@ -15,6 +16,7 @@ def upload_scode():
 
 @app.route('/post', methods=['GET','POST'])
 def post():
+    keys = db.keys()
     if request.method == 'POST':
         the_code = request.form['thecode']
         the_code = str(the_code)
@@ -24,8 +26,15 @@ def post():
         
         if the_code == 'no_code' or code_name == '':
             return redirect('/upload')
+        elif code_name in keys:
+            return redirect('/upload')
         else:
-            return render_template('post.html', value=the_code) # 이 코드 말고 다른것으로 고쳐야됨 (전체 스코드 목록 페이지라던지)
+            db[f'{code_name}'] = the_code
+            return render_template('scodes.html', value=the_code) # 이 코드 말고 다른것으로 고쳐야됨  (정보 db에 올리고 전체 스코드 목록 페이지로 리다이렉트)
+
+@app.route("/scodes")
+def scodes():
+
     
 if __name__ == '__main__':
     app.run()
